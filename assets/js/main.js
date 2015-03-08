134b3f5462b00cbf3e5dc7c1f410c19329873c81
Evan Turner <evturn@gmail.com>
2015-03-07 22:53:29 -0500

    function inside script tag on index moved to script file

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $("#second").bootFolio({
+    bfLayout: "bflayhover",
+    bfFullWidth:"box", 
+    bfHover: "bfhover4",
+    bfAnimation: "scale",
+    bfSpace: "space",
+    bfAniDuration: 500,
+    bfCaptioncolor: "#020202",
+    bfTextcolor:"#ffffff",
+    bfTextalign:"center",
+    bfNavalign:"center"
+  });
+});
