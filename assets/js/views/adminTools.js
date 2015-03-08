e2790185c2a12c5ee26527dbb9a25324353b1685
Evan Turner <evturn@gmail.com>
2015-03-08 09:05:52 -0400

    user icon added to admin tools in nav

--- assets/js/views/adminTools.js
+++ assets/js/views/adminTools.js
@@ -16,11 +16,11 @@ var AdminTools = Backbone.View.extend({
     console.log('we fading!');
 		e.preventDefault();
 		$('#login-form').remove();
+		$('body').animate({scrollTop: '0px'}, 800)
 		newPostNav = new NewPostNav();
 		newPostNav.render();
 		$('#new-post-btn').css({opacity: 0})
-  	$('#new-post-btn').fadeTo({opacity: 1}, 5000);
-
+  	$('#new-post-btn').fadeTo({opacity: 1}, 5000)
 	},
 	render: function() {
 		this.$el.append(this.template());
