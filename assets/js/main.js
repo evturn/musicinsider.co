36d51196f871d9935ec5d9d9e6b63425a20ed30a
Evan Turner <evturn@gmail.com>
2015-03-09 12:22:47 -0400

    removed autofocus from login input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+var ref = new Firebase('https://musicinsider.firebaseio.com/posts');
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
 var adminTools = new AdminTools();
 allPosts = new AllPosts();
 allBlogPosts = new AllBlogPosts({collection: allPosts});
