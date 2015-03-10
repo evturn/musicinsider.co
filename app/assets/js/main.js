0146d287c2f3228fb5e155c35af006ecc1d693c2
Evan Turner <evturn@gmail.com>
2015-03-10 13:19:05 -0400

    testing form uploader out

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -81,4 +81,24 @@ $(function() {
     startWithSlide:0    
   }); 
 
+  var url = 'http://localhost:3000/audio';
+      $('#fileupload').fileupload({
+          url: url,
+          dataType: 'json',
+          done: function (e, data) {
+              $.each(data.result.files, function (index, file) {
+                  $('<p/>').text(file.name).appendTo('#files');
+              });
+          },
+          progressall: function (e, data) {
+              var progress = parseInt(data.loaded / data.total * 100, 10);
+              $('#progress .progress-bar').css(
+                  'width',
+                  progress + '%'
+              );
+          }
+      }).prop('disabled', !$.support.fileInput)
+          .parent().addClass($.support.fileInput ? undefined : 'disabled');
+  });
+
 });
