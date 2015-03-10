a237d75ccd6bca14fbba62437723c9e4d7af30d4
Evan Turner <evturn@gmail.com>
2015-03-10 03:37:03 -0400

    node server running app

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express'),
 		 logger = require('morgan');
+
 var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+
+app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(req, res) {
 	res.render('index.html');
-})
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Listening on port ' + app.get('port'));
+});
