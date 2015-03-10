ea5a127389230b6f87bd5a7c7bc621d4fb73fb17
Evan Turner <evturn@gmail.com>
2015-03-09 20:58:26 -0400

    grabbing post id with click

--- assets/js/views/blogPost.js
+++ assets/js/views/blogPost.js
@@ -1,8 +1,14 @@
 var BlogPost = Backbone.View.extend({
-
 	template: _.template($('#blog-post-template').html()),
 	initialize: function() {
 		this.render();
+	},
+	events: {
+		'click #edit' : 'editPost'
+	},
+	editPost: function() {
+		postId	= this.model.get('id');
+		console.log(postId);
 	},
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
