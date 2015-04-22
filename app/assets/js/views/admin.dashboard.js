b9dc394679c21a454446c073aab42511109b7004
Evan Turner <evturn@gmail.com>
2015-04-22 08:04:20 -0400

    creating backbone view for the admin dashboard

---
+++ app/assets/js/views/admin.dashboard.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+app.Dashboard = Backbone.View.extend({
+	el: '.admin-dashboard',
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.dashboardTemplate());
+		return this;
+	},
+});
