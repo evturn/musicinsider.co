1e743e2d0360fc627384332430102edd432cf886
Evan Turner <evturn@gmail.com>
2015-04-22 08:39:09 -0400

    form for new post rendering in admin-dashboard form-container

--- app/assets/js/views/admin.dashboard.js
+++ app/assets/js/views/admin.dashboard.js
@@ -2,12 +2,21 @@ var app = app || {};
 
 app.Dashboard = Backbone.View.extend({
 	el: '.admin-dashboard',
-	dashboardTemplate: _.template($('#dashboard-template').html()),
+	dashboardTemplate : _.template($('#dashboard-template').html()),
+	formNewTemplate   : _.template($('#form-new-template').html()),
+	formEditTemplate  : _.template($('#form-edit-template').html()),
+	events: {
+		'click btn-form-admin-create' : 'create',
+		'click btn-form-admin-update' : 'update',
+		'click btn-form-admin-delete' : 'delete'
+	},
 	initialize: function() {
 		this.render();
+		this.$el.hide();
 	},
 	render: function() {
 		this.$el.html(this.dashboardTemplate());
+		$('.admin-form-container').html(this.formNewTemplate());
 		return this;
 	},
 });
