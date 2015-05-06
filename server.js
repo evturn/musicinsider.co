14e1928c72b0b728e3ffb772f7517504044cf551
Evan Turner <evturn@gmail.com>
2015-05-06 12:22:31 -0400

    ejs rendering dependencies

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-var express = require('express'),
-		 logger = require('morgan');
+var express = require('express');
+var logger 	= require('morgan');
+var ejs 		= require('ejs');
 
 var app = express();
 
+app.set('views', __dirname + '/public/views');
+
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index.html');
+	res.render('layout');
 });
 
 app.listen(app.get('port'), function() {
