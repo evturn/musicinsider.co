98c03dce9c5748c27d9836ff0a37324fb52eea2e
Evan Turner <evturn@gmail.com>
2015-05-24 20:15:44 -0400

    db config file created

---
+++ config/db.js
@@ -0,0 +1,22 @@
+var mongodb  = require('mongodb');
+var mongoose = require('mongoose');
+
+module.exports = function(mongoose) {
+
+  var dbName = 'music-insider';
+
+  mongoose.connect('mongodb://localhost/' + dbName, function(error) {
+    if (error) {
+      console.log('Ensure you\'re connected');
+    }
+  });
+
+  var db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error: '));
+
+  db.once('open', function callback() {
+    console.log('Connected to ' + dbName);
+  });
+
+};
